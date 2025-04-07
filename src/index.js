eded26f0ca0f05c62f231d0f9258c8b1b7988f66
ranggadwias <ranggadwi783@gmail.com>
2025-04-08 06:07:05 +0700

    rename component files and update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './components/tab-navigation.js';
-import './components/note-item.js';
-import './components/note-list.js';
-import './components/add-button.js';
-import './components/note-form.js';
+import './components/TabNavigation.js';
+import './components/NoteItem.js';
+import './components/NoteList.js';
+import './components/AddButton.js';
+import './components/NoteForm.js';
